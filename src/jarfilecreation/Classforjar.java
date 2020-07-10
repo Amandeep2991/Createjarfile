2291fe96ca0ac802ca95a02d783084ea5631b30f
udaisinghrathore <udaisinghrathore@192.168.1.4>
2020-07-11 00:15:04 +0530

    jar file creation

---
+++ src/jarfilecreation/Classforjar.java
@@ -0,0 +1,27 @@
+package jarfilecreation;
+
+public class Classforjar {
+
+
+
+	
+
+	public static void abc()
+	{
+          System.out.println("This is from Classforjar static file");
+      }
+	public void xyz()
+	{
+		System.out.println("This is Classforjar method2 ");
+	}
+
+	public static void main(String[] args) {
+		Classforjar s1=new Classforjar();
+		s1.abc();
+		s1.xyz();
+		
+		
+		
+	
+	}
+	}
